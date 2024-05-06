edfc0f41a62caf537c4015b49ff36346d18ef20f
Nadesh <nadesh@arkinfotec.com>
2024-05-06 18:58:29 +0530

    input & output labels used

--- main.go
+++ main.go
@@ -13,8 +13,10 @@ func main() {
 	fmt.Println("IP: ", ip)
 
 	vh := videohub.NewVideohub(ip)
-	// Now you can use methods of the Videohub struct, like vh.Route(), vh.InputLabel(), etc.
 
+	// Now you can use methods of the Videohub struct, like vh.Route(), vh.InputLabel(), etc.
 	// Use vh to perform some action, for example:
-	vh.Route(1, 2) // Route output 1 to input 2
+	vh.Route(0, 0) // Route output 1 to input 2
+	vh.InputLabel(1, "Camera 2")
+	vh.OutputLabel(0, "Switcher 1")
 }
